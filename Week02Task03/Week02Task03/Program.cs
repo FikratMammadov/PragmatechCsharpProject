8d7c83693f36a455d61bdc072768f19c1c70fa9d
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-16 17:33:27 +0400

    Week02Task03 added.

---
+++ Week02Task03/Week02Task03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Week02Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil edin: ");
+            int eded = Convert.ToInt32(Console.ReadLine());
+
+            if (eded == 0)
+            {
+                Console.WriteLine("Eded ne tekdir, ne de cutdur.");
+            }
+            else if (eded % 2 == 0)
+            {
+                Console.WriteLine("Eded cutdur.");
+            }
+            else
+            {
+                Console.WriteLine("Eded tekdir.");
+            }
+
+        }
+    }
+}
