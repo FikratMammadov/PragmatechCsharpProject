7d48c5648e6d014396650862de50fd68cd3ec614
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 00:47:07 +0400

    week03task03 added.

---
+++ Week03Task03/Week03Task03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Week03Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndNum = rnd.Next(1, 11);
+            //Console.WriteLine(rndNum);
+            int chance = 5;
+            int userChoice;
+            do
+            {
+                Console.Write("1 ve 10 arasinda bir eded texmin edin: ");
+                userChoice = Convert.ToInt32(Console.ReadLine());
+                chance--;
+    
+                if (chance == 0 && userChoice!=rndNum)
+                {
+                    Console.WriteLine("Sizin texmin haqqiniz bitmisdir.");
+                }
+                else
+                {
+                    if (userChoice == rndNum)
+                    {
+                        Console.WriteLine("Tebrikler siz duz tapdiniz.");
+                        break;
+                    }
+                    else if (userChoice > rndNum)
+                    {
+                        Console.WriteLine("Daxil etdiyiniz eded nezerde tutulan ededden boyukdur.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Daxil etdiyiniz eded nezerde tutulan ededden kicikdir.");
+                    }
+                }
+
+            } while (chance!=0);
+        }
+    }
+}
