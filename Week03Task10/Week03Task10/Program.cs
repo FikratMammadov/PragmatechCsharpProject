395179dd8f8260680b0da99f22e483f2fe6dd061
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 18:16:02 +0400

    week03task10 added.

---
+++ Week03Task10/Week03Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Week03Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char chr = 'a'; chr <='z'; chr++)
+            {
+                Console.Write("{0} ",chr);
+            }
+        }              
+    }
+}
