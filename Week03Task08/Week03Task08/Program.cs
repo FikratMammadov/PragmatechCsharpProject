66772ccba632787e286ec80c79f01e9f3fc1f3d0
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 18:00:25 +0400

    week03task08 added.

---
+++ Week03Task08/Week03Task08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Week03Task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+
+            CheckNumbers:
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int rndNum = rnd.Next(1, 11);
+                numbers[i] = rndNum;
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        rndNum = rnd.Next(1, 11);
+                        numbers[i] = rndNum;
+                        goto CheckNumbers;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array elementleri:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
