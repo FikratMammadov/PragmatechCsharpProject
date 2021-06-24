10acb6738344b44e7d092604f59c9450bdb154d9
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 16:36:48 +0400

    week03task07 added.

---
+++ Week03Task07/Week03Task07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Week03Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] saitler = new char[]{ 'a', 'ı', 'o', 'u', 'e', 'ə', 'i', 'ö', 'ü'};
+            Console.Write("Bir soz daxil edin: ");
+            string soz = Console.ReadLine();
+            int saitSayi = 0;
+
+            for (int i = 0; i < soz.Length; i++)
+            {
+                for (int j = 0; j < saitler.Length; j++)
+                {
+                    if (soz[i]==saitler[j])
+                    {
+                        saitSayi++;
+                    }
+                }
+            }
+
+            if (saitSayi==0)
+            {
+                Console.WriteLine("Bu sozde sait isledilmemisdir.");
+            }
+            else
+            {
+                Console.WriteLine("{0} sait isledilmisdir.", saitSayi);
+            }
+        }
+    }
+}
