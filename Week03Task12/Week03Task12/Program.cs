de2117b13c57b33c16ea69f1cfb73b1456bceeae
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 18:31:11 +0400

    week03task12 added.

---
+++ Week03Task12/Week03Task12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week03Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. ededi daxil edin: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. ededi daxil edin: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} ve {1} arasindaki sade ededler: ",num1,num2);
+            for (int i = num1; i <= num2; i++)
+            {
+                int divisor = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        divisor++;
+                    }
+                }
+
+                if (divisor==2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
