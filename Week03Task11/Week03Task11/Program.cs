b91e998a2e9d10212c90a3a183c77fb762afe5a4
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-24 18:22:38 +0400

    week03task11 added.

---
+++ Week03Task11/Week03Task11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Week03Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            for (int i = 2; i <= 100; i++)
+            {
+                int divisor = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        divisor++;
+                    }
+                }
+
+                if (divisor==2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
