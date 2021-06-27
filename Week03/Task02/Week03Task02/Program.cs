59de61a05cc6aa0e9a509ca93a280baa0c3730fa
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-06-27 23:35:13 +0400

    Week03Task08 and folder structure updated.

---
+++ Week03/Task02/Week03Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week03Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double num = 0;
+            do
+            {
+                Console.Write("Eded daxil edin: ");
+                num = Convert.ToDouble(Console.ReadLine());
+                sum += num;
+            } while (num != 0);
+            Console.WriteLine("Cem: {0}", sum);
+        }
+    }
+}
---
+++ Week03/Task08/Week03Task08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Week03Task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int rndNum = rnd.Next(0, 10);
+                numbers[i] = rndNum;
+                CheckNumbers:
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        rndNum = rnd.Next(0, 10);
+                        numbers[i] = rndNum;
+                        count++;
+                        goto CheckNumbers;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array elementleri:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Tekrar sayi: {0}",count);
+        }
+    }
+}
