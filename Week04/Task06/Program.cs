1965f6bbee6b88b9093cbb4bba5cb696d90b90d1
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-07-01 16:15:21 +0400

    week04task06 added.

---
+++ Week04/Task06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 32, 12, 54, 4, 23, 56, 34, 12, 54 };
+
+            // Usul 1
+            //int min = numbers[0];
+            //int numbersLen = numbers.Length;
+            //for (int i = 0; i < numbersLen; i++)
+            //{
+            //    if (numbers[i]<min)
+            //    {
+            //        min = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine("Minimum: {0}",min);
+
+            // Usul 2
+            int min = numbers.Min();
+            Console.WriteLine("Minimum: {0}", min);
+        }
+    }
+}
