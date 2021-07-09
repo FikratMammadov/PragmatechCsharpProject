dec40fb0e51b1c1d092acabdacb35e149ce6205d
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-07-09 14:11:51 +0400

    week05task04 added.

---
+++ Week05/Task04/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Task04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.countBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(324, 36);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(108, 63);
+            this.addBtn.TabIndex = 5;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // countBtn
+            // 
+            this.countBtn.Location = new System.Drawing.Point(324, 201);
+            this.countBtn.Name = "countBtn";
+            this.countBtn.Size = new System.Drawing.Size(108, 64);
+            this.countBtn.TabIndex = 6;
+            this.countBtn.Text = "Count";
+            this.countBtn.UseVisualStyleBackColor = true;
+            this.countBtn.Click += new System.EventHandler(this.countBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(324, 118);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(108, 64);
+            this.deleteBtn.TabIndex = 7;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(454, 35);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(108, 64);
+            this.searchBtn.TabIndex = 8;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.countBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button countBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button searchBtn;
+    }
+}
+
---
+++ Week05/Task04/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text.Trim().ToUpper();
+            if (!string.IsNullOrEmpty(user))
+            {
+                listBox1.Items.Add(user);
+
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void countBtn_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.Items.Count;
+            MessageBox.Show("Count: "+count.ToString());
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text.Trim().ToUpper();
+
+            if (listBox1.Items.Contains(name))
+            {
+                MessageBox.Show(name+ " liste daxildir.");
+            }
+            else
+            {
+                MessageBox.Show(name + " liste daxil deyil.");
+            }
+        }
+    }
+}
