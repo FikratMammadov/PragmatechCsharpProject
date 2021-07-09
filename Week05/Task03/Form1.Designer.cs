6132cedbadf715e0cb49fff317115fd3135c67cb
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-07-09 13:34:54 +0400

    week05task03 added.

---
+++ Week05/Task03/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Task03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.mixBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(274, 25);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(128, 62);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(274, 111);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(128, 62);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // mixBtn
+            // 
+            this.mixBtn.Location = new System.Drawing.Point(274, 195);
+            this.mixBtn.Name = "mixBtn";
+            this.mixBtn.Size = new System.Drawing.Size(128, 62);
+            this.mixBtn.TabIndex = 5;
+            this.mixBtn.Text = "Mix";
+            this.mixBtn.UseVisualStyleBackColor = true;
+            this.mixBtn.Click += new System.EventHandler(this.mixBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(534, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mixBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button mixBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week05/Task03/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text.Trim().ToLower();
+            if (!string.IsNullOrEmpty(user))
+            {
+                listBox1.Items.Add(user);
+
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void mixBtn_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int listLen = listBox1.Items.Count;
+
+            int[] rndNums = new int[listLen];
+            ArrayList users = new ArrayList();
+
+            for (int i = 0; i < listLen; i++)
+            {
+                int rndNum = rnd.Next(1, listLen+1);
+
+                if (!rndNums.Contains(rndNum))
+                {
+                    rndNums[i] = rndNum;
+                    users.Add(listBox1.Items[rndNums[i]-1].ToString());
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < listLen; i++)
+            {
+                listBox1.Items[i] = users[i];
+            }
+
+        }
+    }
+}
