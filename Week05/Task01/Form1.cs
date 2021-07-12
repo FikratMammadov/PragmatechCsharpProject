fb9ca506c70a28211c593bac022cd123814b0ee2
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-07-12 16:14:05 +0400

    week05task01 updated.

--- Week05/Task01/Form1.cs
+++ Week05/Task01/Form1.cs
@@ -33,13 +33,28 @@ namespace Task01
                     result = num1 - num2;
                     break;
                 case "/":
-                    result = num1 / num2;
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("0-a bolme emeliyyati yoxdur");
+                    }
                     break;
                 case "*":
                     result = num1 * num2;
                     break;
                 case "%":
-                    result = num1 % num2;
+                    if (num2 != 0)
+                    {
+                        result = num1 % num2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("0-a bolme emeliyyati yoxdur");
+                    }
+                     
                     break;
                 default:
                     break;
