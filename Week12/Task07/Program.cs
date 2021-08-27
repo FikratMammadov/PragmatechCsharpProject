52de9c60ea9c813512c9c7d16a6e6ba463a786f8
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 17:46:44 +0400

    week12task07 added.

---
+++ Week12/Task07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hemin file-i acin ve icine metn yazin.Metni yazarken byte[] halina salaraq yazin. 
+            string subFolder = @"D:\CSharpGroup3\SubFolderCSharpGroup3";
+            foreach (var file in Directory.GetFiles(subFolder))
+            {
+                if (Path.GetExtension(file) == ".txt")
+                {
+                    FileStream strm = File.Open(file, FileMode.Open);
+                    string text = "C# is programming language";
+                    byte[] textByte = Encoding.Default.GetBytes(text);
+                    strm.Write(textByte, 0, textByte.Length);
+                    strm.Close();
+                }
+            }
+        }
+    }
+}
