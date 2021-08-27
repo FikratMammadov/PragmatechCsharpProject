d75c4dc9f9e15db5937ca38a5fa6f906f63b2f67
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 17:13:15 +0400

    week12task05 added.

---
+++ Week12/Task05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yaratdiginiz subfolderin icinde iki file yaradin,file-lardan biri word,biri excel formatinda olsun.
+            string subFolder = @"D:\CSharpGroup3\SubFolderCSharpGroup3\";
+            string[] files = new string[] { "textFile.txt", "excelFile.xlsx" };
+
+            foreach (var file in files)
+            {
+                File.Create(subFolder + file);
+            }
+
+
+        }
+    }
+}
