c93dc2de58452cb63e28eeff01b8bc6113040be6
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 17:03:32 +0400

    week12task04 added.

---
+++ Week12/Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subFolder = @"D:\CSharpGroup3\SubFolderCSharpGroup3";
+
+            if (!Directory.Exists(subFolder))
+            {
+                Directory.CreateDirectory(subFolder);
+            }
+            
+        }
+    }
+}
