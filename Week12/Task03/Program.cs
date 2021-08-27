6d372bd0b1b48a9d9819c8f346fdb07d9ccc7a37
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 17:01:23 +0400

    week12task03 added.

---
+++ Week12/Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CSharpGroup3 folderinin icinde SubFolderCSharpGroup3 folderinin olub olmamasini yoxlayin
+            string subFolder = @"D:\CSharpGroup3\SubFolderCSharpGroup3";
+
+            if (Directory.Exists(subFolder))
+            {
+                Console.WriteLine("this folder exists");
+            }
+            else
+            {
+                Console.WriteLine("this folder doesn't exists.");
+            }
+
+        }
+    }
+}
