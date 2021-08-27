1678d38ff637a3e1caa87adb155fd58e2c0a6b15
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 16:56:41 +0400

    week12task02 added.

---
+++ Week12/Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Daha sonra hemin folderin icinde eyni adda txt file - i yaradin.
+            string fileName = @"D:\CSharpGroup3\CSharpGroup3.txt";
+            File.Create(fileName);
+        }
+    }
+}
