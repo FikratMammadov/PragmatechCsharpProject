545d7bbac8858347d5d94972751e9e64c674f503
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-27 17:32:33 +0400

    week12task06 added.

---
+++ Week12/Task06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CSharpGroup3 folderin icinde olan butun file-lari goturun ve extension - i txt olan file - i tapin
+            string subFolder = @"D:\CSharpGroup3\SubFolderCSharpGroup3";
+            foreach (var file in Directory.GetFiles(subFolder))
+            {
+                FileInfo info = new FileInfo(file);
+                if (Path.GetExtension(file)==".txt")
+                {
+                    Console.WriteLine(info.Name);
+                }
+            }
+        }
+    }
+}
